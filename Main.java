a28917610fae27b55e20a90c77ff9a8af5b99eed
Aimar Ibarra <greenerclay@gmail.com>
2024-02-22 08:36:23 +0100

    Initial

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Main here.
+ * 
+ * @author Aimar Ibarra
+ * @version 0.1
+ */
+public class Main
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Main
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
